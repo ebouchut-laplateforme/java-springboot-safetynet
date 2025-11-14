c6945075cf5b1d17038c7c369c1bc2f7a2b05ecc
Eric Bouchut <ebouchut@gmail.com>
2025-11-14 16:33:45 +0100

    feat: Add pom.xml + java sources

---
+++ src/main/java/com/ericbouchut/springboot/safetynet/JavaSpringbootSafetynetApplication.java
@@ -0,0 +1,13 @@
+package com.ericbouchut.springboot.safetynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootSafetynetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootSafetynetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericbouchut/springboot/safetynet/JavaSpringbootSafetynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericbouchut.springboot.safetynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootSafetynetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
